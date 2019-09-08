37487f47218f9f028990acbc83a9ae15d3e174ce
Ansi Zhang <zhangansi@gmail.com>
2019-09-09 01:07:16 +0800

    Revert "增加直接从cnki复制到剪切板内容转换支持"

--- cnki2bibtex/cnki2bib.py
+++ cnki2bibtex/cnki2bib.py
@@ -18,39 +18,30 @@ def copyToClipBoard(content):
 
 
 @click.command()
-@click.argument("inputFile", type=click.Path(exists=True, dir_okay=False), required=False)
+@click.argument("inputFile", type=click.Path(exists=True, dir_okay=False))
 @click.option("--copy/--no-copy", "-c/-nc", default=True, help="Whether or not to copy the result to clipboard. Default: True")
-@click.option("--outputDefault/--no-outputDefault", "-od/-nod", default=True, help="Whether or not to create a default .bib file. It has the same name as the source .net file in its directory. Or if the input source is clipboard, it will be 'out.bib' in current working directory. Default: True")
+@click.option("--outputDefault/--no-outputDefault", "-od/-nod", default=True, help="Whether or not to create a .bib file with the same name as the .net file in its directory. Default: True")
 @click.option("--outputfile", "-o", type=click.File('w', encoding="utf-8"), help="Create a certain output .bib file.")
 @click.option("--id-format", "-f", type=click.Choice(['title', 'nameyear']), help="Choose the format of the ID. Pinyin of the first words in the title, or pinyin of the first author plus year")
 def launch(inputfile, copy, outputdefault, outputfile, id_format):
-    '''Converting a NoteExpress Entry .net file exported by CNKI to BibTeX .bib file.
-
-\b
-Arguments:
-
-    INPUTFILE: Optional. The input .net file to be converted. If left empty, the contents in the clipboard will be used.'''
+    '''Converting a NoteExpress Entry .net file exported by CNKI to BibTeX .bib file.'''
 
     if not copy and not outputdefault and not outputfile:
         click.echo("Why are you calling me ???")
         return
 
-    if inputfile != None:
-        if os.path.splitext(inputfile)[1] != ".net":
-            logging.warning(
-                "The input file may not be a NoteExpress Entries file.")
+    if os.path.splitext(inputfile)[1] != ".net":
+        logging.warning(
+            "The input file may not be a NoteExpress Entries file.")
 
-        if id_format:
-            setIDFormat(id_format)
+    if id_format:
+        setIDFormat(id_format)
 
-        try:
-            with open(inputfile, 'r', encoding="utf-8") as f:
-                cnkiNetFileContent = f.read()
-        except:
-            click.echo("Failed to open the file. Please check input path.")
-    else:
-        click.echo("Read the NoteExpress Entry content from the clipboard.")
-        cnkiNetFileContent = pyperclip.paste()
+    try:
+        with open(inputfile, 'r', encoding="utf-8") as f:
+            cnkiNetFileContent = f.read()
+    except:
+        click.echo("Failed to open the file. Please check input path.")
 
     try:
         bibFileString = getBibFileContentString(cnkiNetFileContent)
@@ -68,19 +59,14 @@ Arguments:
 
     if outputdefault:
         try:
-            targetPath = (os.path.splitext(inputfile)[
-                          0] if inputfile else "out") + ".bib"
+            targetPath = os.path.splitext(inputfile)[0] + ".bib"
             with open(targetPath, "w", encoding="utf-8") as f:
                 f.write(bibFileString)
-
-            if inputfile:
-                click.echo(
-                    "File '{}' is created at the same directory as the source file.".format(os.path.basename(targetPath)))
-            else:
-                click.echo("File 'out.bib' is created at current directory.")
+            click.echo(
+                "File '{}' is created at the same directory as the source file.".format(os.path.basename(targetPath)))
         except Exception as e:
             click.echo(
-                "Failed to write the default output .bib file.")
+                "Failed to write a .bib file at the same directory as the source file.")
             click.echo("Error message:\n" + str(e))
 
     if outputfile:
